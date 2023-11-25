3f0a665b90fa9aa4be440548999042f92385f047
manish5124 <manishchouhan5124@gmail.com>
2023-11-25 12:38:20 +0530

    update if condition in useEffect of context

--- src/Context.js
+++ src/Context.js
@@ -19,20 +19,38 @@ const ContextProvider = ({ children }) => {
   const userVideo = useRef();
   const connectionRef = useRef();
 
+  // useEffect(() => {
+  //   navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+  //     .then((currentStream) => {
+  //       setStream(currentStream);
+
+  //       myVideo.current.srcObject = currentStream;
+  //     });
+
+  //   socket.on('me', (id) => setMe(id));
+
+  //   socket.on('callUser', ({ from, name: callerName, signal }) => {
+  //     setCall({ isReceivingCall: true, from, name: callerName, signal });
+  //   });
+  // }, []);
+
   useEffect(() => {
     navigator.mediaDevices.getUserMedia({ video: true, audio: true })
       .then((currentStream) => {
         setStream(currentStream);
-
-        myVideo.current.srcObject = currentStream;
+  
+        if (myVideo.current) {
+          myVideo.current.srcObject = currentStream;
+        }
       });
-
+  
     socket.on('me', (id) => setMe(id));
-
+  
     socket.on('callUser', ({ from, name: callerName, signal }) => {
       setCall({ isReceivingCall: true, from, name: callerName, signal });
     });
   }, []);
+  
 
   const answerCall = () => {
     setCallAccepted(true);
